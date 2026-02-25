f1246690ad2ce64d27a6b34a66de49878a1ecc27
WvdB <wybe.vandenbosch+git@gmail.com>
2026-02-25 16:18:31 +0100

    fix: rename file paths

--- show-the-data/src/main.jsx
+++ show-the-data/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import MewgenicsCats from '../mewgenics-cats.jsx';
+import MewgenicsCats from './mewgenics-cats.jsx';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<MewgenicsCats />);
